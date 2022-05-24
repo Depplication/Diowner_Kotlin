ccd05fc20b56302037fa0b439a21b5363f282b4f
bhindor <82446373+bhindor@users.noreply.github.com>
2022-05-24 16:07:08 +0900

    이용약관 동의 구현 중.. (렉 때문에 한번 끊어요)

--- app/src/main/java/kr/hs/diowner/JoinActivity.kt
+++ app/src/main/java/kr/hs/diowner/JoinActivity.kt
@@ -6,10 +6,15 @@ import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.ScrollView
-import android.widget.TextView
+import android.widget.*
 
 class JoinActivity : AppCompatActivity() {
+    val RuleCheck : CheckBox by lazy {
+        findViewById(R.id.check)
+    }
+    val JoinBtn : Button by lazy {
+        findViewById(R.id.Join_Btn)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
@@ -20,6 +25,18 @@ class JoinActivity : AppCompatActivity() {
         tv_rule?.setOnTouchListener { v, event ->
             sv?.requestDisallowInterceptTouchEvent(true)//부모 scroll 권한 빼는 부분
             return@setOnTouchListener false
+        }
+        tv_rule?.setOnScrollChangeListener { view, scrollX, scrollY, oldScrollX, oldScrolly ->
+            if (tv_rule?.canScrollVertically(1)) {
+                RuleCheck.isEnabled = true
+            }
+        }
+        JoinBtn.setOnClickListener {
+            if(RuleCheck.isChecked){
+                Toast.makeText(this, "동의 하셨습니다", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "이용약관에 동의해 주세요", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
