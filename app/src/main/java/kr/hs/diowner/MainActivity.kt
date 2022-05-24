d11ac71f156e3538f9383fa7066d0a9c87c36809
bhindor <82446373+bhindor@users.noreply.github.com>
2022-05-25 08:45:39 +0900

    Main 화면 진행중

--- app/src/main/java/kr/hs/diowner/MainActivity.kt
+++ app/src/main/java/kr/hs/diowner/MainActivity.kt
@@ -1,11 +1,39 @@
 package kr.hs.diowner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    val My_Btn : Button by lazy {
+        findViewById(R.id.my_btn)
+    }
+    val Register_Btn : Button by lazy {
+        findViewById(R.id.register)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        settingListener()
+    }
+    private fun settingListener(){
+        My_Btn.setOnClickListener(this)
+        Register_Btn.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?){
+        when(view){
+            My_Btn -> {
+                val intent = Intent(this, MyActivity::class.java)
+                startActivity(intent)
+            }
+            Register_Btn -> {
+                val intent = Intent(this, Register_Btn::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
