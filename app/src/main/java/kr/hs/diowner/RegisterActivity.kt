44a30b24fc2544db654f8dd4cda3b9a44e887fa0
bhindor <82446373+bhindor@users.noreply.github.com>
2022-05-26 15:19:48 +0900

    광고등록 화면 진행중

--- app/src/main/java/kr/hs/diowner/RegisterActivity.kt
+++ app/src/main/java/kr/hs/diowner/RegisterActivity.kt
@@ -2,10 +2,41 @@ package kr.hs.diowner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kr.hs.diowner.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_register)
+
+        val itemList = listOf("선택하세요", "신규이벤트", "할인이벤트", "기본이벤트")
+        val adapter = ArrayAdapter(this, R.layout.activity_register, itemList)
+        binding.eventSp.adapter = adapter
+        binding.eventSp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position != 0) Toast.makeText(
+                    this@RegisterActivity,
+                    itemList[position],
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
     }
 }
