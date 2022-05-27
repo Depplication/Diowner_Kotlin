13cffbe14bb3c3a3dce106aab25ceb20754da4ae
bhindor <82446373+bhindor@users.noreply.github.com>
2022-05-27 10:43:43 +0900

    캘린더 진행중

--- app/src/main/java/kr/hs/diowner/RegisterActivity.kt
+++ app/src/main/java/kr/hs/diowner/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package kr.hs.diowner
 
 import android.app.DatePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +13,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 import kr.hs.diowner.databinding.ActivityRegisterBinding
 import java.util.*
 
@@ -25,7 +30,57 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+        setSpinner()
+        showdate()
+    }
 
+    private fun showdate() {
+        val calenderConstraintBuilder = CalendarConstraints.Builder()
+        calenderConstraintBuilder.setValidator(DateValidatorPointForward.now())
+        val sharedPreference = getSharedPreferences("setDate", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreference.edit()
+        binding.firstDayBtn.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("select_first_day")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(calenderConstraintBuilder.build())
+
+            val datePicker = builder.build()
+
+            datePicker.addOnPositiveButtonClickListener {
+                val calendar = Calendar.getInstance()
+                calendar.time = Date(it)
+                val calendarMilli = calendar.timeInMillis
+                binding.tvStartDay.text = "${calendar.get(Calendar.YEAR)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.DAY_OF_MONTH)}"
+                editor.putLong("first_date", calendarMilli)
+                editor.commit()
+                Log.d("Die_Millis", "$calendarMilli")
+            }
+            datePicker.show(supportFragmentManager, datePicker.toString())
+        }
+        val calenderConstraintBuilder2 = CalendarConstraints.Builder()
+        calenderConstraintBuilder2.setValidator(DateValidatorPointForward.from(sharedPreference.getLong("first_date", 0)))
+        binding.endDayBtn.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("select_end_day")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(calenderConstraintBuilder2.build())
+
+            val datePicker = builder.build()
+
+            datePicker.addOnPositiveButtonClickListener {
+                val calendar = Calendar.getInstance()
+                calendar.time = Date(it)
+                val calendarMilli = calendar.timeInMillis
+                binding.tvEndDay.text = "${calendar.get(Calendar.YEAR)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.DAY_OF_MONTH)}"
+                editor.putLong("end_date", calendarMilli)
+                editor.apply()
+                Log.d("Die_Millis", "$calendarMilli")
+            }
+            datePicker.show(supportFragmentManager, datePicker.toString())
+        }
+    }
+    private fun setSpinner(){
         val items = resources.getStringArray(R.array.itemList)
 
         val myAapter = object : ArrayAdapter<String>(this, R.layout.item_spinner) {
@@ -66,14 +121,13 @@ class RegisterActivity : AppCompatActivity() {
             ) {
 
                 when (position) {
-                    0 -> {
-                        Toast.makeText(this@RegisterActivity, "Open event", Toast.LENGTH_SHORT).show()
-                    }
-                    1 -> {
-                        Toast.makeText(this@RegisterActivity, "sale event", Toast.LENGTH_SHORT).show()
-                    }
-                    2 -> Toast.makeText(this@RegisterActivity, "store event", Toast.LENGTH_SHORT).show()
+                    0 -> Log.d("test", position.toString())
+
+                    1 -> Log.d("test", position.toString())
+
+                    2 -> Log.d("test", position.toString())
                     else -> {
+                        Log.d("test_error", position.toString())
                     }
                 }
             }
@@ -82,26 +136,7 @@ class RegisterActivity : AppCompatActivity() {
                 Log.d("MyTag", "onNothingSelected")
             }
         }
-        showdate()
     }
-
-    private fun showdate() {
-        binding.firstDayBtn.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(this, { _, year, month, day ->
-                binding.tvStartDay.text =
-                    year.toString() + "/" + (month + 1).toString() + "/" + day.toString()
-            }, year, month, day)
-            datePickerDialog.show()
-        }
-        binding.endDayBtn.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(this, { _, year, month, day ->
-                binding.tvEndDay.text =
-                    year.toString() + "/" + (month + 1).toString() + "/" + day.toString()
-            }, year, month, day)
-            datePickerDialog.show()
-        }
-    }
-
     private fun dipToPixels(dipValue: Float): Float {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
