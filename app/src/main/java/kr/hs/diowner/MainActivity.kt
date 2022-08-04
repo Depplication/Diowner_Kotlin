62a1527c0a50fd61b84e2b5c3b618a80101d1eca
bhindor <82446373+bhindor@users.noreply.github.com>
2022-08-04 19:33:21 +0900

    main_activity

--- app/src/main/java/kr/hs/diowner/MainActivity.kt
+++ app/src/main/java/kr/hs/diowner/MainActivity.kt
@@ -7,42 +7,31 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kr.hs.diowner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    val My_Btn : Button by lazy {
-        findViewById(R.id.my_btn)
-    }
-    val Register_Btn : Button by lazy {
-        findViewById(R.id.register)
-    }
-    val name : TextView by lazy {
-        findViewById(R.id.name_id)
-    }
-    val nowPoint : TextView by lazy {
-        findViewById(R.id.nowPoint)
-    }
+   private lateinit var binding: ActivityMainBinding
     private var backpressedTime : Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         settingListener()
-        name.text = "익명 아이디"
-        nowPoint.text = "8P"
     }
     private fun settingListener(){
-        My_Btn.setOnClickListener(this)
-        Register_Btn.setOnClickListener(this)
+        binding.myLayout.setOnClickListener(this)
+        binding.adLayout.setOnClickListener(this)
     }
 
     override fun onClick(view: View?){
         when(view){
-            My_Btn -> {
+            binding.myLayout -> {
                 val intent = Intent(this, MyActivity::class.java)
                 startActivity(intent)
             }
-            Register_Btn -> {
+            binding.adLayout -> {
                 val intent = Intent(this, RegisterActivity::class.java)
                 startActivity(intent)
             }
