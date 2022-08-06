f28cca7a21a067246f07fe8a87fcb4e5786ee20f
bhindor <82446373+bhindor@users.noreply.github.com>
2022-08-06 14:37:40 +0900

    menu 구현중..

--- app/src/main/java/kr/hs/diowner/MainActivity.kt
+++ app/src/main/java/kr/hs/diowner/MainActivity.kt
@@ -3,16 +3,18 @@ package kr.hs.diowner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.View
 import android.widget.Button
+import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import kr.hs.diowner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-   private lateinit var binding: ActivityMainBinding
-    private var backpressedTime : Long = 0L
+    private lateinit var binding: ActivityMainBinding
+    private var backpressedTime: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,13 +23,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         settingListener()
     }
-    private fun settingListener(){
+
+    private fun settingListener() {
         binding.myLayout.setOnClickListener(this)
         binding.adLayout.setOnClickListener(this)
     }
 
-    override fun onClick(view: View?){
-        when(view){
+    override fun onClick(v: View?) {
+        when (v) {
             binding.myLayout -> {
                 val intent = Intent(this, MyActivity::class.java)
                 startActivity(intent)
@@ -40,10 +43,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onBackPressed() {
-        if(System.currentTimeMillis() - backpressedTime >= 1500 ){
+        if (System.currentTimeMillis() - backpressedTime >= 1500) {
             backpressedTime = System.currentTimeMillis()
             Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
-        }else{
+        } else {
             finish()
         }
     }
--- app/src/main/java/kr/hs/diowner/MyActivity.kt
+++ app/src/main/java/kr/hs/diowner/MyActivity.kt
@@ -3,12 +3,16 @@ package kr.hs.diowner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.LinearLayoutManager
 import kr.hs.diowner.data.PointData
 import kr.hs.diowner.databinding.ActivityMyBinding
 
-class MyActivity : AppCompatActivity(), View.OnClickListener {
+class MyActivity : AppCompatActivity(), View.OnClickListener, PopupMenu.OnMenuItemClickListener {
     private lateinit var binding: ActivityMyBinding
 
     val mDatas = mutableListOf<PointData>(
@@ -27,14 +31,28 @@ class MyActivity : AppCompatActivity(), View.OnClickListener {
         settingListener()
     }
 
-    private fun settingListener(){
+    private fun settingListener() {
         binding.settingImg.setOnClickListener(this)
         binding.myAdLayout.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         //TODO 메뉴구현하기
+        when (v) {
+            binding.settingImg -> {
+                showPopup(binding.settingImg)
+            }
+        }
         //TODO 내 광고 화면 만들고 이어주기
+    }
+
+    private fun showPopup(v: View) {
+        val themeWrapper = ContextThemeWrapper(this, R.style.PopupMenu)
+        val popup = PopupMenu(themeWrapper, v, Gravity.NO_GRAVITY, 0, R.style.PopupMenu)
+        popup.menu.add(0, 0,0, "정보수정")
+        popup.menu.add(0, 1, 1, "로그아웃")
+        popup.setOnMenuItemClickListener(this)
+        popup.show()
     }
 
     private fun initDataRecyclerView() {
@@ -47,5 +65,16 @@ class MyActivity : AppCompatActivity(), View.OnClickListener {
     private fun addDataRecyclerView() {
         //TODO 서버에서 값받아오기
         //mDatas.add(서버값)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            0 -> Intent(this, ModifyActivity::class.java).run {
+                startActivity(this) }
+            1 -> Intent(this, LoginActivity::class.java).run {
+                startActivity(this)
+                finish() }
+        }
+        return item != null
     }
 }
