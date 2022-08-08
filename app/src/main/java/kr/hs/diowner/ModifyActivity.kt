ff276e6a3301c6f39b265195a7561d40acca6d96
bhindor <82446373+bhindor@users.noreply.github.com>
2022-08-08 10:18:22 +0900

    정보변경 xml

--- app/src/main/java/kr/hs/diowner/ModifyActivity.kt
+++ app/src/main/java/kr/hs/diowner/ModifyActivity.kt
@@ -2,10 +2,16 @@ package kr.hs.diowner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.hs.diowner.databinding.ActivityModifyBinding
 
 class ModifyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityModifyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modify)
+        binding = ActivityModifyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvName.bringToFront()
     }
 }
