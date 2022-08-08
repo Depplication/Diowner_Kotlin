156b48180e58a804568a2c1060e966cfb8fcbdd2
bhindor <82446373+bhindor@users.noreply.github.com>
2022-08-08 10:23:35 +0900

    정보수정 xml 구현중

--- app/src/main/java/kr/hs/diowner/ModifyActivity.kt
+++ app/src/main/java/kr/hs/diowner/ModifyActivity.kt
@@ -2,9 +2,10 @@ package kr.hs.diowner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kr.hs.diowner.databinding.ActivityModifyBinding
 
-class ModifyActivity : AppCompatActivity() {
+class ModifyActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityModifyBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,28 @@ class ModifyActivity : AppCompatActivity() {
         binding = ActivityModifyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvName.bringToFront()
+        settingView()
+        settingListener()
     }
+
+    private fun settingView(){
+        binding.tvName.bringToFront()
+        binding.tvShopName.bringToFront()
+        binding.tvPhoneNum.bringToFront()
+        binding.tvAddress.bringToFront()
+        binding.tvBank.bringToFront()
+        binding.tvAccount.bringToFront()
+    }
+    private fun settingListener(){
+        binding.backBtn.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        when(v) {
+            binding.backBtn -> {
+                finish()
+            }
+        }
+    }
+
+
 }
