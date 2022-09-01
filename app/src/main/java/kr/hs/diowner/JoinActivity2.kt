2c697c94a07b22b7b312fcf920bde4dec2b70a1c
jogayeon <jogayeon0113@gmail.com>
2022-09-01 17:21:33 +0900

    Add: 회원가입 추가

--- app/src/main/java/kr/hs/diowner/JoinActivity2.kt
+++ app/src/main/java/kr/hs/diowner/JoinActivity2.kt
@@ -9,26 +9,18 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.*
+import kr.hs.diowner.databinding.ActivityJoin2Binding
 import kr.hs.diowner.databinding.ActivityJoinBinding
 import java.util.regex.Pattern
 
-class JoinActivity : AppCompatActivity() {
-    lateinit var binding: ActivityJoinBinding
+class JoinActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityJoin2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
 
-        binding.NextBtn1.setOnClickListener {
-
-            var intent = Intent(this, JoinActivity2::class.java);
-            intent.putExtra("name", binding.RName1.text.toString());
-            intent.putExtra("id", binding.userId1.text.toString());
-            intent.putExtra("PW1", binding.PW1.text.toString());
-            intent.putExtra("PW2", binding.CPW1.text.toString());
-            startActivity(intent);
-        }
 
 
     }
