3f07599a0f49691dc2fd08ed334409bf6092fff8
LucyMoon <rjf5907@daum.net>
2022-10-14 11:47:32 +0900

    Modify: 서버 주소 바뀜

--- app/src/main/java/kr/hs/diowner/RetrofitBuilder.kt
+++ app/src/main/java/kr/hs/diowner/RetrofitBuilder.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitBuilder {
     val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
     var api: API
-    private var serverIP: String = "61.97.187.175:8080/dion/"
+    private var serverIP: String = "10.80.163.87:3309/"
 
     init {
         val retrofit = Retrofit.Builder()
